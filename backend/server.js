c6ee18da5ca30c75798f19e000f7644bb3c9120c
Ajay Pisharody <43316414+ajaypisharody@users.noreply.github.com>
2020-12-02 10:22:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import userRouter from './routers/userRouter.js';
 import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import orderRouter from './routers/orderRouter.js';
-const path =require('path');
+import path from 'path';
 dotenv.config();
 
 const app = express();
