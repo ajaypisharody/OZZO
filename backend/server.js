63c4cd9de96214a58deb5288fa59a43e8222086a
Ajay Pisharody <43316414+ajaypisharody@users.noreply.github.com>
2020-12-02 10:42:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) =>{
 if(process.env.NODE_ENV === 'production'){
 	app.use(express.static('frontend/build'));
 	app.get('*', (req, res)=>{
-		res.sendFile(path.resolve('/', 'index.html'));
+		res.sendFile(path.resolve('OZZO/', 'index.html'));
 	});
 
 }
